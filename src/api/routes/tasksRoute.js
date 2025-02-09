35639a4083480be9fe2a06410a2b2cdb757c0cd4
Balint <118969351+bujdosobalintlaszlo@users.noreply.github.com>
2025-02-09 22:10:07 +0100

    Add files via upload

--- src/api/routes/tasksRoute.js
+++ src/api/routes/tasksRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getCardInfo, getSpecificCard, getTaskCount, getRandomTask, getTaskWithSearch, getTasksByDifficult} = require('../controllers/tasksController');
+const { getCardInfo, getSpecificCard, getTaskCount, getRandomTask, getTaskWithSearch, getTasksByDifficult,getTaskByThemes} = require('../controllers/tasksController');
 
 router.get('/get-cards-info/:offset', getCardInfo);
 router.get('/get-one-card/:id',getSpecificCard)
@@ -8,5 +8,6 @@ router.get('/taskCount',getTaskCount);
 router.get('/random-task',getRandomTask);
 router.get('/task-with-search/:characters',getTaskWithSearch)
 router.get('/task-with-difficulty/:difficulty',getTasksByDifficult)
+router.get('/filter-task-by-themes/:themes', getTaskByThemes);
 module.exports = router;
 
