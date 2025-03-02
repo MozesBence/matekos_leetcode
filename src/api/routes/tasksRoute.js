333db1d7b576a53db335b445f8756ce257a92a15
Balint <118969351+bujdosobalintlaszlo@users.noreply.github.com>
2025-03-02 13:11:41 +0100

    Update tasksRoute.js

--- src/api/routes/tasksRoute.js
+++ src/api/routes/tasksRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getCardInfo, getSpecificCard, getTaskCount, getRandomTask, getTaskWithSearch, getTasksByDifficult,getTaskByThemes} = require('../controllers/tasksController');
+const { getCardInfo, getSpecificCard, getTaskCount, getRandomTask, getTaskWithSearch, getTasksByDifficult,getTaskByThemes, getFilteredTasks} = require('../controllers/tasksController');
 
 router.get('/get-cards-info/:offset', getCardInfo);
 router.get('/get-one-card/:id',getSpecificCard)
@@ -9,5 +9,5 @@ router.get('/random-task',getRandomTask);
 router.get('/task-with-search/:characters',getTaskWithSearch)
 router.get('/task-with-difficulty/:difficulty',getTasksByDifficult)
 router.get('/filter-task-by-themes/:themes', getTaskByThemes);
+router.get('/filteredTasks', getFilteredTasks);
 module.exports = router;
-
