43d775ac8b8bd09dc4fedf31473216a7769dec0b
Bálint László Bujdosó <bujdosobalint8@gmail.com>
2025-03-27 17:38:46 +0100

    feladatok feltoltese

--- src/api/models/advertisement_cards.js
+++ src/api/models/advertisement_cards.js
@@ -36,9 +36,9 @@ module.exports = (sequelize) => {
         const items = [
             { title: "Vágj bele egy versenybe!", content: "Inspirálódj és mérd össze tudásodat másokkal – lépj be a kihívásba!", button_title: "Kezdés", redirect: "/contest" },
             { title: "Tanulós zene válogatás", content: "Válogattunk neked olyan zenéket, amiket mi, fejlesztők, szívesen hallgatunk tanulás közben. Engedd, hogy a ritmus motiváljon!", button_title: "Zene", redirect: "https://music.youtube.com/browse/VLPL9bxdY6QqIVXPlbrfbUQGjRw3kWJS6nc7" },
-            { title: "Költsd el az aranzaid!", content: "Költsd el aranyaid limitált merchekre vagy oldalon belül használható tárgyakra!", button_title: "Irány az áruház", redirect: "/store" },
+            { title: "Költsd el az aranyaidat!", content: "Költsd el aranyaid limitált merchekre vagy oldalon belül használható tárgyakra!", button_title: "Irány az áruház", redirect: "/store" },
             { title: "Nézz rá a communityra!", content: "Csatlakozz a közösséghez, találkozz hasonló gondolkodású emberekkel, és építsd tovább tudásodat!", button_title: "Community", redirect: "/community" },
-            {title: "Küldj be feladatos, és tegyél hozzá a közösséghez!",content:"Ha a feladatod, elfogadásra kerül ay adminok által, az oldalon a feladato a teved lesy megjelölve.",button_title:"Feldat beküldése",redirect:"/task-creation"}
+            { title: "Küldj be feladatot, és járulj hozzá a közösséghez!",content: "Ha a feladatodat elfogadják az adminok, az oldalon a feladatnál a te neved lesz feltüntetve.",button_title: "Feladat beküldése",redirect: "/task-creation"},
         ];
 
 
