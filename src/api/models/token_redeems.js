2a0054bc8d8418b81bdb02c67e91a862bb43eeaa
Bálint László Bujdosó <bujdosobalint8@gmail.com>
2025-03-27 21:45:07 +0100

    token redeem model fix

--- src/api/models/token_redeems.js
+++ src/api/models/token_redeems.js
@@ -1,38 +1,32 @@
-const { Model, DataTypes } = require('sequelize');
+const {Model} = require('sequelize')
 
-module.exports = (sequelize) => {
-    class TokenRedeems extends Model {
-        static init(sequelize) {
-            super.init(
-                {
-                    user_id: {
-                        type: DataTypes.INTEGER,
-                        allowNull: false,
-                    },
-                    task_id: {
-                        type: DataTypes.INTEGER,
-                        allowNull: false,
-                    },
-                    submission_date: {
-                        type: DataTypes.DATE,
-                        allowNull: false,
-                        defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
-                    },
-                    state: {
-                        type: DataTypes.ENUM('active', 'inactive'),
-                        allowNull: false,
-                    },
-                },
-                {
-                    sequelize,
-                    modelName: 'TokenRedeems',
-                    tableName: 'tokenRedeems',
-                    timestamps: false,
-                }
-            );
-        }
-    }
+module.exports = (sequelize,DataTypes) => {
+    class TokenRedeems extends Model {}
 
-    TokenRedeems.init(sequelize);
+    TokenRedeems.init({
+        user_id:{
+            type: DataTypes.INTEGER,
+            allowNull:false
+        },
+        task_id: {
+            type:DataTypes.INTEGER,
+            allowNull:false
+        },
+        submission_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+        },
+        state: {
+            type: DataTypes.ENUM('active', 'inactive'),
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        modelName:"TokenRedeems",
+        tableName:'tokenRedeems',
+        timestamps:false
+    })
     return TokenRedeems;
-};
+}
