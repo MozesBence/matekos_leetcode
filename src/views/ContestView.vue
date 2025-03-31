114661ea53fe545c0dad14e2636dc77a2a03f4a2
DanyiPV <pinteadani88@gmail.com>
2025-03-31 22:25:31 +0200

    some more fix

--- src/views/ContestView.vue
+++ src/views/ContestView.vue
@@ -194,7 +194,7 @@ function getTimeUntilNextMonth() {
 function formatTimeDifference(ms) {
   const totalSeconds = Math.floor(ms / 1000);
   const days = Math.floor(totalSeconds / 86400), hours = Math.floor((totalSeconds % 86400) / 3600),
-        minutes = Math.floor((totalSeconds % 3600) / 60), seconds = totalSeconds % 60;
+  minutes = Math.floor((totalSeconds % 3600) / 60), seconds = totalSeconds % 60;
   return `${days} nap ${hours} óra ${minutes} perc ${seconds} mp`;
 }
 
